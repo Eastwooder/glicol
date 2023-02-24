249a234da462683af296cc6a8a80585ade16d5e2
chaosprint <chaosprint@gmail.com>
2023-02-24 09:42:58 +0100

    log in readme

--- rs/parser2/src/lib.rs
+++ rs/parser2/src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use pest::Parser;
+use pest::error::Error;
+use pest_derive::*;
+// use pest::iterators::Pair;
+// use pest::error::ErrorVariant;
+use hashbrown::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use glicol_macros::{one_para_number_or_ref, two_numbers};
+use glicol_synth::GlicolPara;
+// extern crate meval;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Parser)]
+#[grammar = "glicol.pest"]
+pub struct GlicolParser;
+
+/// index, (vector of chain name, vector of parameter list)
+pub type GlicolAst = HashMap<String, (Vec<String>, Vec<Vec<GlicolPara>>)>;
+
+pub fn get_ast(code: &str) -> Result<GlicolAst, Error<Rule>> {
+    let mut block = match GlicolParser::parse(Rule::block, code) {
+        Ok(v) => v,
+        Err(e) => { return Err(e) }
+    };
+    // this can be a comment though, but we call it a line
+    let lines = block.next().unwrap();
+    let mut ast = GlicolAst::new();
+    ast
 }
--- rs/synth2/README.md
+++ rs/synth2/README.md
@@ -8,7 +8,34 @@ Do we need to limit such a situation: add a mono node inside a stereo context.
 I think we should, although it means some extra expense on the performance.
 
 ## Todos
-- [] provide some Error enums
-- [] handle the channel issues better
+- [x] provide some Error enums
+- [x] handle the channel issues better
 - [] make it easier to specify the input
 - [] write the test better (previously we rely on the plotting things; maybe we add `cpal` this time)
+
+
+## Thoughts
+
+#### 20230218
+A better graph constructor is needed.
+
+```rust
+
+fn main() -> Result<(), GlicolSynthError> {
+
+    let mut graph = GlicolGraph::new();
+    graph.insert("o", vec![GlicolNode::SinOsc(vec![440.0])]);
+
+    let mut context = GlicolContextBuilder::<4>::new()
+    .sr(44100)
+    .channels(2)
+    .set_graph(graph)
+    .build();
+
+    println!("first block {:?}", context.next_block());
+    context.send_msg(node_a, GlicolMessage::SetToNumber(0, 100.) );
+    println!("2nd block {:?}", context.next_block());
+    Ok(())
+}
+
+```
